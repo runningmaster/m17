88b30190df12e8583f1dd091d653b1b4b6d298eb
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-04-06 16:14:12 +0300

    main: add project's scheme

--- src/main/doc.go
+++ src/main/doc.go
@@ -1,4 +1,31 @@
 package main
 
+/*
+
+  +----------+
+  |          |-+
+  | internal | |-+   Common util packages
+  |          | | |
+  +----------+ | |
+   +-----------+ |
+     +-----------+
+
+  +----------+
+  |          |-+
+  |   main   | |-+  Application domain packages
+  |          | | |
+  +----------+ | |
+   +-----------+ |
+     +-----------+
+
+  +----------+
+  |          |-+
+  |  vendor  | |-+  Third-party packages
+  |          | | |
+  +----------+ | |
+   +-----------+ |
+     +-----------+
+*/
+
 // common pattern:
 // main -> command(flags, env) -> some deps -> router -> api -> server.ListenAndServe()
