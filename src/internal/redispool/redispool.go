eff5b25830514ee3e15cf59fa20d70585a9df3f1
Dmitriy Kovalenko <runningmaster@gmail.com>
2017-04-26 00:25:45 +0300

    redispool: remove context funcs

--- src/internal/redispool/redispool.go
+++ src/internal/redispool/redispool.go
@@ -1,7 +1,6 @@
 package redispool
 
 import (
-	"context"
 	"net/url"
 	"time"
 
@@ -78,21 +77,4 @@ func Wait(w bool) func(*redis.Pool) error {
 		p.Wait = w
 		return nil
 	}
-}
-
-// contextClientRedisKey is a unique type to prevent assignment.
-// Its associated value should be a redis.Pool.
-type contextClientRedisKey struct{}
-
-// ContextWithRedisPool returns a new context based on the provided parent ctx.
-func ContextWithRedisPool(ctx context.Context, v *redis.Pool) context.Context {
-	return context.WithValue(ctx, contextClientRedisKey{}, v)
-}
-
-// RedisConnFromContext returns the redis.Conn value associated with the given key.
-func RedisConnFromContext(ctx context.Context) redis.Conn {
-	if v, ok := ctx.Value(contextClientRedisKey{}).(*redis.Pool); ok {
-		return v.Get()
-	}
-	panic("*redis.Pool not found")
 }
